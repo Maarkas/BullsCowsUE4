f96728a42388c1e333acaa6c465c77c589624c4b
Markas <markas.mikas@gmail.com>
2021-01-08 03:04:18 +0200

    Bull cow count

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -2,6 +2,7 @@
 #include "BullCowCartridge.h"
 #include "Misc/FileHelper.h"
 #include "Misc/Paths.h"
+#include "Math/UnrealMathUtility.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
@@ -11,14 +12,14 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     SetupGame();
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString& PlayerInput) // When the player hits enter
 {
     if (bGameOver)
     {
         ClearScreen();
         SetupGame();
     } else {
-        ProcessGuess(Input);
+        ProcessGuess(PlayerInput);
     }
 }
 
@@ -27,17 +28,17 @@ void UBullCowCartridge::SetupGame()
     /* Welcome Screen */
     PrintLine(TEXT("Welcome to the Bull Cow Game!"));
 
-    HiddenWord = Words[rand() % Words.Num()];
+    HiddenWord = Words[FMath::RandRange(0, Words.Num() - 1)];
     Lives = HiddenWord.Len();
     bGameOver = false;
 
     PrintLine(TEXT("The word is made of %i letters!"), HiddenWord.Len());
     PrintLine(TEXT("You have %i tries!"), Lives);
     PrintLine(TEXT("Press Return to start playing!"));
-    PrintLine(TEXT("The HiddenWord is: %s, length of %i!"), *HiddenWord, HiddenWord.Len());
+    // PrintLine(TEXT("The HiddenWord is: %s, length of %i!"), *HiddenWord, HiddenWord.Len());
 }
 
-void UBullCowCartridge::ProcessGuess(FString Guess)
+void UBullCowCartridge::ProcessGuess(const FString& Guess)
 {
     if (HiddenWord == Guess) 
     {
@@ -48,6 +49,7 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
     if(IsIsogram(Guess))
     {
         PrintLine(TEXT("No repeating letters, guess again!"));
+        return;
     }
 
     --Lives;
@@ -61,10 +63,10 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
     if (HiddenWord.Len() != Guess.Len())
     {
         PrintLine(TEXT("Your word must be %i letters long!"), HiddenWord.Len());
-        return;
     }
 
-    
+    FBullCowCount Score = GetBullCows(Guess);
+    PrintLine(TEXT("Bulls: %i, Cows: %i"), Score.Bulls, Score.Cows);
 }
 
 void UBullCowCartridge::EndGame()
@@ -82,7 +84,7 @@ void UBullCowCartridge::EndGame()
     PrintLine(TEXT("Press Enter to play again!"));
 }
 
-bool UBullCowCartridge::IsIsogram(FString Word) const
+bool UBullCowCartridge::IsIsogram(const FString& Word) const
 {
     for (int32 i = 0; i < Word.Len(); i++)
     {
@@ -95,4 +97,27 @@ bool UBullCowCartridge::IsIsogram(FString Word) const
         }
     }
     return false;
+}
+
+FBullCowCount UBullCowCartridge::GetBullCows(const FString& Guess) const
+{
+    FBullCowCount Count;
+    for (int32 i = 0; i < Guess.Len(); i++)
+    {
+        if (Guess[i] == HiddenWord[i])
+        {
+            Count.Bulls++;
+            continue;
+        }
+        
+        for (int32 j = 0; j < Guess.Len(); j++)
+        {
+            if (Guess[j] == HiddenWord[j])
+            {
+                Count.Cows++;
+                break;
+            }
+        }
+    }
+    return Count;
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -6,23 +6,30 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
 	public:
-	virtual void BeginPlay() override;
-	virtual void OnInput(const FString& Input) override;
-	void SetupGame();
-	void EndGame();
-	void ProcessGuess(FString Guess);
-	bool IsIsogram(FString Word) const;
+		virtual void BeginPlay() override;
+		virtual void OnInput(const FString& Input) override;
+		void SetupGame();
+		void EndGame();
+		void ProcessGuess(const FString& Guess);
+		bool IsIsogram(const FString& Word) const;
+		FBullCowCount GetBullCows(const FString& Guess) const;
 
 	// Your declarations go below!
 	private:
-    FString HiddenWord;
-	int32 Lives;
-	bool bGameOver;
-	TArray<FString> Words;
+		FString HiddenWord;
+		int32 Lives;
+		bool bGameOver;
+		TArray<FString> Words;
 };
